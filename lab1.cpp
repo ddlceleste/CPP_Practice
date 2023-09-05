23bf5376370f5a3cfc0ec8b7d30e4c2974f1a458
Alex <alexanderthomatis@icloud.com>
2023-09-05 12:42:49 -0700

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	string name;
+	int age;
+
+	cout << "Hello what is your name:" << endl;
+	cin >> name;
+
+	cout << "What is your age: " << endl;
+	cin >> age;
+
+	cout << "Your name is " << name << "." << endl;
+	cout << "You are " << age << "years old." << endl;
+
+
+	return 0;
+}
