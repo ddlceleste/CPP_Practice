e382d8c732b0bc8484d919b61242cfac8632932d
Alex <alexanderthomatis@icloud.com>
2023-09-07 13:31:33 -0700

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+	float temp = 0.0f;
+
+	cout << "Enter the temperature in your current location: ";
+	cin >> temp;
+	cout << '\n';
+
+	if(temp <= 70.0f){
+
+		cout << "Bring a jacket!" << endl;
+	}
+	else{
+
+		cout << "Have an awesome trip!" << endl;
+	}
+
+	return 0;
+}
