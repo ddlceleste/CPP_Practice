b55287fef31776de630c45df95bd2b5d1057c824
Alex <alexanderthomatis@icloud.com>
2023-09-12 13:36:57 -0700

    Update lab8_partB.cpp

--- lab8_partB.cpp
+++ lab8_partB.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+	int num1 = 0;
+	int num2 = 0;
+	bool flag;
+
+	cout << "Enter Num1: "; cin >> num1;
+	cout << "Enter Num2: "; cin >> num2;
+
+	flag = false;
+
+	if(num1 > 0 && num2 < 0){
+
+		flag = true;
+	}
+	else if(num1 == -5 && num2 > 0){
+
+		flag = true;
+	}
+
+
+	if(flag){
+
+		cout << "Complex condition met!" << endl;
+	}
+
+
+	return 0;
+}
