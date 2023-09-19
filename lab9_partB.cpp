264e14d5b168e2b70f7dfcf39b90a1dc636c92f0
Alex <alexanderthomatis@icloud.com>
2023-09-19 13:50:09 -0700

    Update lab9_partB.cpp

--- lab9_partB.cpp
+++ lab9_partB.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	string message;
+	cout << "Enter a message :";
+	getline(cin, message);
+
+	while(!(message.length() >= 5 && message.length() <= 10)){
+
+		cout << "Invalid! Message must have between 5 and 10 characters." << '\n';
+		cout << "Enter a message :";
+		getline(cin, message);
+	}
+
+	int counter = message.length();
+
+	cout << "Countdown initiating . . ." << '\n';
+	while(!(counter <= 0)){
+
+		cout << counter << ", ";
+		counter--;
+	}
+
+	cout << '\n' << "Blast off to Planet " << message << '!';
+
+
+	return 0;
+}
 
